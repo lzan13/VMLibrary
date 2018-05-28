3e98f7c17e6bb49ed63343157a06f6ba9aeac6cf
lz liu <lzan13@lzdeMacBook-Pro.local>
2018-05-28 11:43:00 +0800

    docs:添加开源协议声明

--- README.md
+++ README.md
@@ -31,3 +31,20 @@ dependencies {
 - [example](./example) 示例 Module 主要都是一些自己测试代码
 - [vmntools](./vmntools) 封装 jni 底层方法的库，暂时只是一些测试代码
 - [vmtools](./vmtools) 自己封装的工具类库，包含常用工具类以及一些自定义控件等
+
+### #LICENSE
+```
+Copyright 2018 lzan13
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+```
--- vmtools/src/main/java/com/vmloft/develop/library/tools/utils/bitmap/VMBlur.java
+++ vmtools/src/main/java/com/vmloft/develop/library/tools/utils/bitmap/VMBlur.java
@@ -33,8 +33,7 @@ public class VMBlur {
         try {
             rs = RenderScript.create(context);
             rs.setMessageHandler(new RenderScript.RSMessageHandler());
-            Allocation input = Allocation.createFromBitmap(rs, overlay,
-                    Allocation.MipmapControl.MIPMAP_NONE, Allocation.USAGE_SCRIPT);
+            Allocation input = Allocation.createFromBitmap(rs, overlay, Allocation.MipmapControl.MIPMAP_NONE, Allocation.USAGE_SCRIPT);
             Allocation output = Allocation.createTyped(rs, input.getType());
 
             ScriptIntrinsicBlur blur = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs));
@@ -54,18 +53,18 @@ public class VMBlur {
     /**
      * Stack Blur v1.0 from
      * http://www.quasimondo.com/StackBlurForCanvas/StackBlurDemo.html
-     * <p>
+     *
      * Java Author: Mario Klingemann <mario at quasimondo.com>
      * http://incubator.quasimondo.com
      * created Feburary 29, 2004
      * Android port : Yahel Bouaziz <yahel at kayenko.com>
      * http://www.kayenko.com
      * ported april 5th, 2012
-     * <p>
+     *
      * This is a compromise between Gaussian Blur and Box blur
      * It creates much better looking blurs than Box Blur, but is
      * 7x faster than my Gaussian Blur implementation.
-     * <p>
+     *
      * I called it Stack Blur because this describes best how this
      * filter works internally: it creates a kind of moving stack
      * of colors whilst scanning through the image. Thereby it
@@ -74,10 +73,10 @@ public class VMBlur {
      * colors on the topmost layer of the stack are either added on
      * or reduced by one, depending on if they are on the right or
      * on the left side of the stack.
-     * <p>
+     *
      * If you are using this algorithm in your code please add
      * the following line:
-     * <p>
+     *
      * Stack Blur Algorithm by Mario Klingemann <mario@quasimondo.com>
      * ------------------------------------------------------------------
      * java层进行模糊，使用堆栈计算模糊法
