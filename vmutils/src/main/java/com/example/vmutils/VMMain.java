94779f98a4ba65caf297ac5a635e464bc11c7224
liulizheng <liulizheng@weidian.com>
2019-03-21 15:29:35 +0800

    测试正则匹配标签和@ 的情况

--- vmutils/src/main/java/com/example/vmutils/VMMain.java
+++ vmutils/src/main/java/com/example/vmutils/VMMain.java
@@ -11,8 +11,9 @@ public class VMMain {
     public static void main(String[] args) {
         System.out.println("lzan13 - run java main");
 
-        String content = "#海贼王# #ONEPIECE# 第876话:仁义的#男子汉# #甚平#拼死的大海流通道";
-        String regStr = "#([\\S]*?)#";
+        String content = "#海贼王# #ONEPIECE# 第876话:仁义的#男子汉# #甚平# #大海 流通道";
+//        String regStr = "#([\\S]*?)#";
+        String regStr = "#[\\u4e00-\\u9fa5a-zA-Z0-9_-]{2,30}";
         new VMMain().regContent(content, regStr);
     }
 
