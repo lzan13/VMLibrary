dce6a4bba1143a0a18d57b30ffca8a8347e35a3e
liulizheng <liulizheng@weidian.com>
2019-04-19 15:49:14 +0800

    更新 VMToast 内部实现

--- example/src/main/java/com/vmloft/develop/library/example/demo/custom/CustomViewActivity.java
+++ example/src/main/java/com/vmloft/develop/library/example/demo/custom/CustomViewActivity.java
@@ -1,10 +1,12 @@
 package com.vmloft.develop.library.example.demo.custom;
 
 import android.view.View;
+import android.widget.Toast;
 
 import com.vmloft.develop.library.example.R;
 import com.vmloft.develop.library.example.common.AppActivity;
 import com.vmloft.develop.library.tools.widget.VMTimerBtn;
+import com.vmloft.develop.library.tools.widget.VMToast;
 
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -28,11 +30,17 @@ public class CustomViewActivity extends AppActivity {
 
     }
 
-    @OnClick({R.id.custom_btn_timer})
+    @OnClick({R.id.custom_btn_timer, R.id.btn_toast_1, R.id.btn_toast_2})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.custom_btn_timer:
                 mTimerBtn.startTimer();
+                break;
+            case R.id.btn_toast_1:
+                VMToast.make("测试 toast ").show();
+                break;
+            case R.id.btn_toast_2:
+                VMToast.make("测试 error toast ").showError();
                 break;
         }
     }
--- vmtools/src/main/java/com/vmloft/develop/library/tools/widget/VMToast.java
+++ vmtools/src/main/java/com/vmloft/develop/library/tools/widget/VMToast.java
@@ -1,21 +1,15 @@
 package com.vmloft.develop.library.tools.widget;
 
-import android.content.Context;
-import android.graphics.PixelFormat;
-import android.graphics.drawable.Drawable;
-import android.os.Build;
-import android.os.Handler;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.graphics.drawable.DrawableCompat;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.vmloft.develop.library.tools.R;
 import com.vmloft.develop.library.tools.VMTools;
+import com.vmloft.develop.library.tools.utils.VMDimen;
 import com.vmloft.develop.library.tools.utils.VMStr;
 
 /**
@@ -24,151 +18,99 @@ import com.vmloft.develop.library.tools.utils.VMStr;
  */
 public class VMToast {
 
-    private Context mContext = VMTools.getContext();
     private static VMToast instance;
-    private boolean isShow;
+    private static String mToastMsg;
+    private static int mToastDuration = Toast.LENGTH_SHORT;
 
-    private Handler handler = new Handler();
+    private Toast mToast;
 
-    private WindowManager windowManager;
-    private WindowManager.LayoutParams toastParams;
-    private View toastView;
-    private ImageView iconView;
-    private TextView textView;
-    private static String toastMsg;
+    private View mView;
+    private ImageView mIconView;
+    private TextView mContentView;
 
 
     private VMToast() {
-        initParams();
-        isShow = false;
-        windowManager = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
-        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        toastView = inflater.inflate(R.layout.vm_widget_toast, null);
-        iconView = toastView.findViewById(R.id.vm_img_toast_icon);
-        textView = toastView.findViewById(R.id.vm_text_toast_text);
+        LayoutInflater inflater = LayoutInflater.from(VMTools.getContext());
+        mView = inflater.inflate(R.layout.vm_widget_toast, null);
+        mIconView = mView.findViewById(R.id.vm_img_toast_icon);
+        mContentView = mView.findViewById(R.id.vm_text_toast_content);
+
+        mToast = new Toast(VMTools.getContext());
+        int offset = VMDimen.dp2px(16);
+        mToast.setGravity(Gravity.RIGHT | Gravity.TOP, 0, offset);
     }
 
+
     /**
-     * 初始化 Toast 布局属性参数：位置，大小等
+     * 根据资源 id 弹出 toast
+     *
+     * @param resId    toast 要展示消息资源 Id
+     * @param duration 显示时长
      */
-    private void initParams() {
-        toastParams = new WindowManager.LayoutParams();
-        toastParams.width = WindowManager.LayoutParams.WRAP_CONTENT;
-        toastParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
-        // Toast 的进入和退出动画效果
-        toastParams.windowAnimations = R.style.VMToastAnim;
-
-        toastParams.format = PixelFormat.TRANSLUCENT;
-        toastParams.type = WindowManager.LayoutParams.TYPE_TOAST;
-        toastParams.flags = WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE;
-        toastParams.gravity = Gravity.LEFT | Gravity.TOP;
-        toastParams.x = mContext.getResources().getDimensionPixelSize(R.dimen.vm_dimen_16);
-        toastParams.y = mContext.getResources().getDimensionPixelSize(R.dimen.vm_dimen_72);
-
+    public static VMToast make(int resId, int duration) {
+        mToastDuration = duration;
+        return make(resId);
     }
 
     /**
      * 根据资源 id 弹出 toast
+     *
+     * @param resId toast 要展示消息资源 Id
      */
     public static VMToast make(int resId) {
-        if (instance == null) {
-            instance = new VMToast();
-        }
-        toastMsg = VMStr.strByResId(resId);
-        return instance;
+        return make(VMStr.strByResId(resId));
     }
 
     /**
      * 根据字符串弹出 toast
+     *
+     * @param msg      toast 要展示的消息
+     * @param duration 显示时长
+     */
+    public static VMToast make(String msg, int duration) {
+        mToastDuration = duration;
+        return make(msg);
+    }
+
+    /**
+     * 根据字符串弹出 toast
+     *
+     * @param msg toast 要展示的消息
      */
     public static VMToast make(String msg) {
         if (instance == null) {
             instance = new VMToast();
         }
-        toastMsg = msg;
+        mToastMsg = msg;
         return instance;
+    }
+
+    /**
+     * 显示操作正确执行完成的 Toast
+     */
+    public void show() {
+        mView.setBackgroundResource(R.drawable.vm_bg_toast_green);
+        mIconView.setImageResource(R.drawable.vm_ic_emoticon);
+        show(mToastMsg);
     }
 
     /**
      * 显示操作有错误提示 Toast
      */
     public void showError() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            iconView.setImageResource(R.drawable.vm_ic_close_24dp);
-            iconView.setColorFilter(ContextCompat.getColor(VMTools.getContext(), R.color.vm_red));
-        } else {
-            Drawable drawable = ContextCompat.getDrawable(VMTools.getContext(), R.drawable.vm_ic_close_24dp);
-            Drawable wrapDrawable = DrawableCompat.wrap(drawable).mutate();
-            wrapDrawable.setBounds(0, 0, drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
-            DrawableCompat.setTint(drawable, ContextCompat.getColor(VMTools.getContext(), R.color.vm_red));
-            iconView.setImageDrawable(wrapDrawable);
-        }
-        show(toastMsg);
-    }
-
-    /**
-     * 显示操作正确执行完成的 oast
-     */
-    public void showDone() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            iconView.setImageResource(R.drawable.vm_ic_done_24dp);
-            iconView.setColorFilter(ContextCompat.getColor(VMTools.getContext(), R.color.vm_green));
-        } else {
-            Drawable drawable = ContextCompat.getDrawable(VMTools.getContext(), R.drawable.vm_ic_done_24dp);
-            Drawable wrapDrawable = DrawableCompat.wrap(drawable).mutate();
-            wrapDrawable.setBounds(0, 0, drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
-            DrawableCompat.setTint(drawable, ContextCompat.getColor(VMTools.getContext(), R.color.vm_green));
-            iconView.setImageDrawable(wrapDrawable);
-        }
-        show(toastMsg);
+        mView.setBackgroundResource(R.drawable.vm_bg_toast_red);
+        mIconView.setImageResource(R.drawable.vm_ic_emoticon_sad);
+        show(mToastMsg);
     }
 
     /**
      * 公共的显示方法
      */
-    private void show(final String msg) {
-        if (!isShow) {
-            isShow = true;
-            textView.setText(msg);
-            windowManager.addView(toastView, toastParams);
-            handler.postDelayed(removeToastRunnable, 3000);
-        } else {
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    removeCallbacks();
-                    removeToast();
-                    show(msg);
-                }
-            }, 2000);
-        }
+    private void show(String msg) {
+        mContentView.setText(msg);
+        mToast.setDuration(mToastDuration);
+        mToast.setView(mView);
+        mToast.show();
     }
 
-    /**
-     * 移除延迟任务
-     */
-    private void removeCallbacks() {
-        handler.removeCallbacks(removeToastRunnable);
-    }
-
-
-    /**
-     * 移除 Toast 任务
-     */
-    private Runnable removeToastRunnable = new Runnable() {
-        @Override
-        public void run() {
-            removeToast();
-        }
-    };
-
-    /**
-     * 移除 Toast
-     */
-    private void removeToast() {
-        if (toastView != null && toastView.isShown()) {
-            windowManager.removeView(toastView);
-        }
-        isShow = false;
-    }
 }
